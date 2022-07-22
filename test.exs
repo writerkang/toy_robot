b21ed2e3c7279950c1ce7a3f1a21c49f65acc92b
writerkang <goodwriterkang@gmail.com>
2022-07-22 16:40:36 +0900

    initial commit

---
+++ test.exs
@@ -0,0 +1,18 @@
+alias ToyRobot.Robot
+
+%Robot{east: 0, north: 0, facing: :north}
+|> Robot.move
+|> Robot.turn_right
+|> Robot.move
+|> Robot.turn_left
+|> IO.inspect
+|> Robot.move
+|> Robot.move
+|> Robot.turn_left
+|> Robot.move
+|> Robot.turn_right
+|> Robot.turn_right
+|> Robot.move
+|> Robot.move
+|> Robot.move
+|> IO.inspect
